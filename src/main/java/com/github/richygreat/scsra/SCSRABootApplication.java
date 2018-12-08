315eb6109341b1b612f6ee8f7f1ee97e849f83c3
Richy Great <richygreat@gmail.com>
2018-12-08 18:47:56 +0100

    Initial Commit

---
+++ src/main/java/com/github/richygreat/scsra/SCSRABootApplication.java
@@ -0,0 +1,11 @@
+package com.github.richygreat.scsra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SCSRABootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SCSRABootApplicationTest.class, args);
+	}
+}
---
+++ src/test/java/com/github/richygreat/scsra/SCSRABootApplicationTest.java
@@ -0,0 +1,4 @@
+package com.github.richygreat.scsra;
+
+public class SCSRABootApplicationTest {
+}
